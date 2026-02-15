bdad64dfd01267a6a24e615b4ff146e54ad961c4
Muhammad Abdullah Zia <abdullah2006zia@gmail.com>
2026-02-15 12:28:03 +0500

    Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,40 +1,25 @@
-#include <iostream>
-#include "Student.h"
-#include <vector>
+#include<iostream>
+#include"College.h"
 using namespace std;
 
-vector<Student> students;
-int rollCounter = 1001;
-
-void addStudent() {
-    string name; int deptChoice, section;
-    cout << "Enter Name: "; cin.ignore(); getline(cin,name);
-    cout << "Department(0-CS,1-Eng,2-Med): "; cin >> deptChoice;
-    cout << "Section: "; cin >> section;
-    Student s(name, static_cast<Department>(deptChoice), section);
-    for(int i=0;i<s.subjects.size();i++){
-        cout << "Marks for " << s.subjects[i] << ": ";
-        cin >> s.marks[i];
-    }
-    s.calculatePercentage();
-    s.calculateScholarship();
-    students.push_back(s);
-}
-
-void displayStudents() {
-    for(auto &s: students){
-        cout << s.rollNo << " " << s.name << " Dept:" << s.dept << " Sec:" << s.section
-             << " %:" << s.percentage << " Sch:" << s.scholarship << "%\n";
-    }
-}
-
 int main(){
+    College c;
     int choice;
     do{
-        cout << "1.Add 2.Display 0.Exit: "; cin >> choice;
+        cout<<"1.Add 2.DisplayAll 3.ByDept 4.Top 0.Exit:";cin>>choice;
         switch(choice){
-            case 1: addStudent(); break;
-            case 2: displayStudents(); break;
+            case 1:c.addStudent();break;
+            case 2:c.displayAll();break;
+            case 3:{
+                int d;cout<<"Dept(0-CS,1-Eng,2-Med):";cin>>d;
+                c.displayByDepartment(static_cast<Department>(d));
+                break;
+            }
+            case 4:{
+                int n;cout<<"Top N students:";cin>>n;
+                c.displayTopStudents(n);
+                break;
+            }
         }
     }while(choice!=0);
 }
