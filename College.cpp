dec721a2bbc563de991dac01b8aed7f15783d3aa
Muhammad Abdullah Zia <abdullah2006zia@gmail.com>
2026-02-15 12:27:06 +0500

    College.cpp

--- College.cpp
+++ College.cpp
@@ -1,29 +1,65 @@
-#include "Student.h"
-#include <iostream>
-#include <vector>
+#include"College.h"
+#include<iostream>
 using namespace std;
 
-extern vector<Student> students;
-extern int rollCounter;
-
-Student::Student(string n, Department d, int sec) {
-    name = n; dept = d; section = sec; rollNo = rollCounter++;
-    if(dept == CS) subjects = {"OOP","DSA","Math"};
-    else if(dept == Engineering) subjects = {"Physics","Math","Engineering"};
-    else subjects = {"Biology","Chemistry","Physics"};
-    marks.resize(subjects.size(), 0);
-    percentage = 0;
-    scholarship = 0;
+Student::Student(string n,Department d,int sec,int roll){
+    name=n;dept=d;section=sec;rollNo=roll;
+    if(dept==CS) subjects={"OOP","DSA","Math"};
+    else if(dept==Engineering) subjects={"Physics","Math","Engineering"};
+    else subjects={"Biology","Chemistry","Physics"};
+    marks.resize(subjects.size(),0);
+    percentage=0;
+    scholarship=0;
 }
 
-void Student::calculatePercentage() {
-    int total = 0;
-    for(int i=0;i<marks.size();i++) total += marks[i];
-    percentage = total * 1.0 / marks.size();
+void Student::enterMarks(){
+    cout<<"Enter marks for "<<name<<":\n";
+    for(int i=0;i<subjects.size();i++){
+        cout<<subjects[i]<<":";
+        cin>>marks[i];
+    }
 }
 
-void Student::calculateScholarship() {
-    if(percentage>90) scholarship=50;
-    else if(percentage>75) scholarship=25;
+void Student::calculatePercentage(){
+    int total=0;
+    for(int m:marks) total+=m;
+    percentage=total*1.0/marks.size();
+}
+
+void Student::calculateScholarship(){
+    if(percentage>=90) scholarship=50;
+    else if(percentage>=75) scholarship=25;
     else scholarship=0;
+}
+
+void Student::display() const{
+    cout<<"Roll:"<<rollNo<<" Name:"<<name
+        <<" Dept:"<<dept<<" Sec:"<<section
+        <<" %:"<<percentage<<" Sch:"<<scholarship<<"%\n";
+}
+
+void College::addStudent(){
+    string n;int sec,deptChoice;
+    cout<<"Enter student name:";cin.ignore();getline(cin,n);
+    cout<<"Department(0-CS,1-Eng,2-Med):";cin>>deptChoice;
+    cout<<"Section:";cin>>sec;
+    Student s(n,static_cast<Department>(deptChoice),sec,rollCounter++);
+    s.enterMarks();
+    s.calculatePercentage();
+    s.calculateScholarship();
+    students.push_back(s);
+}
+
+void College::displayAll(){
+    for(auto &s:students) s.display();
+}
+
+void College::displayByDepartment(Department d){
+    for(auto &s:students) if(s.getDept()==d) s.display();
+}
+
+void College::displayTopStudents(int n){
+    vector<Student> temp=students;
+    sort(temp.begin(),temp.end(),[](Student &a,Student &b){return a.getPercentage()>b.getPercentage();});
+    for(int i=0;i<n && i<temp.size();i++) temp[i].display();
 }
